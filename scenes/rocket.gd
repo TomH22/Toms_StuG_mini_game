6cc38f3de84d54ea44166fac75088aa59180ea31
Tom H <t.hennersdorf@flexijet.info>
2024-01-05 15:34:08 +0100

    - add sound - add android export - set width and height to my display size - fix rocket position error after first restart

--- scenes/rocket.gd
+++ scenes/rocket.gd
@@ -6,12 +6,15 @@ var explosion_scene = preload("res://scenes/explosion.tscn")
 @onready var tank_pos : Vector2
 @onready var balistic_easy_simulation : float
 @onready var bes_v : Vector2
+@onready var start_pos : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	start_pos = position
 	init()
 
 func init():
+	position = start_pos
 	started = 0
 	tank_pos = Vector2(0, 0)
 	balistic_easy_simulation = 0.0
@@ -22,22 +25,23 @@ func init():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if started == 1:
+		visible = true
 		var a = (tank_pos - global_position).normalized()
 		a = (a + bes_v).normalized()
 		bes_v = bes_v * (1.0 - min(10.0 * delta, 1.0))
 		
 		var dist = tank_pos.distance_to(global_position)
 		print(dist)	
-		var speed = 12000
+		var speed = 6000
 
 		if dist < 50:
-			speed = 500
+			speed = 250
 		elif dist < 100:
-			speed = 1500
+			speed = 750
 		elif dist < 250:
-			speed = 3000
+			speed = 1500
 		elif dist < 500:
-			speed = 6000
+			speed = 3000
 		
 		global_position = global_position + a * delta * speed
 		global_rotation = a.angle() + PI
